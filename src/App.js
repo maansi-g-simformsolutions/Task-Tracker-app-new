67629bacd02cf113363f8822a088ffb816ba9ef1
Maansi <75968003+maansi-g-simformsolutions@users.noreply.github.com>
2021-04-08 00:36:59 +0530

    Update Task

--- src/App.js
+++ src/App.js
@@ -1,77 +1,85 @@
-import React, { useState, useEffect} from "react";
-import {TextField, Button } from "@material-ui/core";
-import Task from './components/Task';
+import React, { useState, useEffect } from "react";
+import { TextField, Button } from "@material-ui/core";
+import Task from "./components/Task";
 import "./App.css";
-import './styles.css';
-import db from './firebase';
-import firebase from 'firebase';
-import Paper from '@material-ui/core/Paper';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import "./styles.css";
+import db from "./firebase";
+import firebase from "firebase";
 
 export default function App() {
 
-  const theme = createMuiTheme({
-    palette: {
-      type: 'dark',
-    },
-  });
-
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [tasks, setTasks] = useState([]);
 
   const addTasks = (event) => {
     event.preventDefault();
-    
-    db.collection('tasks').add({
+
+    db.collection("tasks").add({
       task: input,
       timestamp: firebase.firestore.FieldValue.serverTimestamp()
-    })
-    setInput('');
-  }
+    });
+    setInput("");
+  };
 
   useEffect(() => {
-    db.collection('tasks').orderBy('timestamp', 'desc').onSnapshot(snapshot => {
-      setTasks(snapshot.docs.map(doc => ({
-        id: doc.id,
-        task: doc.data().task
-      })))
-    })
-  }, [])
+    db.collection("tasks")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setTasks(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            task: doc.data().task
+          }))
+        );
+      });
+  }, []);
 
   return (
-    <ThemeProvider theme={theme} style={{backgroundSize: 'cover'}}>
-    <Paper>
-    <div className="area" >
-            <ul className="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
-    
-    <div className="App">
-      <div className="task-container">
-        <h1>Task Tracker App 📗</h1>
-        <br />
-        <form>
-        <TextField value={input} onChange={event => setInput(event.target.value)} id="standard-basic" label="✅ Write a Task" />
-        <Button type='submit' disabled={!input} onClick={addTasks} variant="contained" style={{background: '#4cd137', border: '2px solid #4cd137', fontWeight: 'bold'}}>
-          ADD TASK
-        </Button>
-        </form>
-        {
-          tasks.map(task => <Task task={task}/>)
-        }
+    <div className="area">
+      <ul className="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+
+      <div className="App">
+        <div className="task-container">
+
+          <h1>Task Tracker App 📗</h1>
+          <br />
+          <form>
+            <TextField
+              value={input}
+              onChange={(event) => setInput(event.target.value)}
+              id="standard-basic"
+              label="✅ Write a Task"
+            />
+            <Button
+              type="submit"
+              disabled={!input}
+              onClick={addTasks}
+              variant="contained"
+              style={{
+                background: "#4cd137",
+                border: "2px solid #4cd137",
+                fontWeight: "bold"
+              }}
+            >
+              ADD TASK
+            </Button>
+          </form>
+          {tasks.map((task) => (
+            <Task task={task} />
+          ))}
+        </div>
       </div>
     </div>
-    </div >
-    </Paper>
-    </ThemeProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
 
 import App from "./App";
+
+const theme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <Paper style={{ height: "100vh", width: "100%" }}>
+        <App />
+      </Paper>
+    </ThemeProvider>
   </StrictMode>,
   rootElement
 );

4ed300e016e10bcf7ced9264d5322fdce0bee220
Maansi <75968003+maansi-g-simformsolutions@users.noreply.github.com>
2021-04-08 00:42:44 +0530

    Update Task

--- src/index.js
+++ src/index.js
@@ -1,24 +1,12 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
 
 import App from "./App";
-
-const theme = createMuiTheme({
-  palette: {
-    type: "dark"
-  }
-});
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <Paper style={{ height: "100vh", width: "100%" }}>
         <App />
-      </Paper>
-    </ThemeProvider>
   </StrictMode>,
   rootElement
 );
